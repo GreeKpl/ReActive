9a65b28eabb284c6868a6bfb38e1f2aed77dddd1
jacek <jacek@jacek-samsung>
2014-11-04 00:15:43 +0100

    Resolve problem with min date.

--- app/src/main/java/pl/edu/agh/inz/reactive/DatabaseManager.java
+++ app/src/main/java/pl/edu/agh/inz/reactive/DatabaseManager.java
@@ -295,16 +295,15 @@ public class DatabaseManager {
 
         Map<Long, Integer> achievements = new TreeMap<Long, Integer>();
 
-        long minDate = 0;
+        long minDate = getMinDate(login);
+        System.out.println("MIN DATE: "+minDate);
 
         if (cursor != null && cursor.moveToFirst()) {
-            minDate = cursor.getLong(0)/(24*60*60*1000);
             achievements.put(minDate, cursor.getInt(1));
         }
 
         while (cursor.moveToNext()) {
-            achievements.put(cursor.getLong(0)/(24*60*60*1000) - minDate, cursor.getInt(1));
-//            System.out.println("+++++ " + minDate + " " + (cursor.getLong(0)/(24*60*60*1000) - minDate));
+            achievements.put(cursor.getLong(0) - minDate, cursor.getInt(1));
         }
         cursor.close();
 
