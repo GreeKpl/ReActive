4286777501dc81a639e6ef472bbd0c8d01fab785
Aleksander <grkalk@gmail.com>
2014-11-15 14:02:08 +0100

    level configuration and backgrounds sent by the client

--- app/src/main/java/pl/edu/agh/inz/reactive/games/rainbow/RainbowSpecification.java
+++ app/src/main/java/pl/edu/agh/inz/reactive/games/rainbow/RainbowSpecification.java
@@ -10,10 +10,38 @@ import static pl.edu.agh.inz.reactive.games.rainbow.RainbowGame.Level;
 public class RainbowSpecification implements GameSpecification {
 
     private Level[] levels = new Level[] {
-            new Level.Builder(6, 5).bg(R.drawable.rainbow_bg_notepad1).targets(1, 0.3, 6, R.drawable.lodka).build(),
+            new Level.Builder(6, 20).bg(R.drawable.rainbow_bg_notepad1).targets(1, 0.3, 6, R.drawable.lodka).build(),
+            new Level.Builder(6, 15).bg(R.drawable.rainbow_bg_notepad1).targets(1, 0.3, 6, R.drawable.lodka).build(),
+            new Level.Builder(6, 10).bg(R.drawable.rainbow_bg_notepad1).targets(1, 0.3, 6, R.drawable.lodka).build(),
+            new Level.Builder(6, 10).bg(R.drawable.rainbow_bg_notepad1).targets(1, 0.3, 6, R.drawable.lodka).build(),
+            new Level.Builder(6, 10).bg(R.drawable.rainbow_bg_notepad1).targets(1, 0.3, 6, R.drawable.lodka).build(),
             new Level.Builder(8, 3).bg(R.drawable.rainbow_bg_cook1).targets(1, 0.2, 3, R.drawable.rainbow_t_cook1).build(),
-            new Level.Builder(10, 4).bg(R.drawable.morze).targets(1, 0.1, 3, R.drawable.lodka).build(),
-            new Level.Builder(15, 4).bg(R.drawable.rainbow_bg_moto1).targets(2, 0.15, 2, R.drawable.rainbow_t_moto2).others(15, 0.1, R.drawable.lodka).build()
+            new Level.Builder(8, 3).bg(R.drawable.rainbow_bg_cook1).targets(1, 0.2, 3, R.drawable.rainbow_t_cook1).build(),
+            new Level.Builder(8, 3).bg(R.drawable.rainbow_bg_4).targets(1, 0.2, 3, R.drawable.rainbow_t_cook1).build(),
+            new Level.Builder(8, 3).bg(R.drawable.rainbow_bg_4).targets(1, 0.2, 3, R.drawable.rainbow_t_cook1).build(),
+            new Level.Builder(8, 3).bg(R.drawable.rainbow_bg_4).targets(1, 0.2, 3, R.drawable.rainbow_t_cook1).build(),
+            new Level.Builder(15, 4).bg(R.drawable.rainbow_bg_moto1).targets(2, 0.15, 2, R.drawable.rainbow_t_moto2).others(15, 0.1, R.drawable.lodka).build(),
+            new Level.Builder(15, 4).bg(R.drawable.rainbow_bg_moto1).targets(2, 0.15, 2, R.drawable.rainbow_t_moto2).others(15, 0.1, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_2).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_2).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_2).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_1).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_1).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_1).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_1).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_1).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_3).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_3).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_3).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_3).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_3).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_bytecode).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_bytecode).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_bytecode).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_bytecode).targets(1, 0.1, 3, R.drawable.lodka).build(),
+            new Level.Builder(10, 4).bg(R.drawable.rainbow_bg_bytecode).targets(1, 0.1, 3, R.drawable.lodka).build(),
+
+
     };
 
     public RainbowGame.Level[] getLevels() {
